8b7d880a73084db938a880f193a2a3a6aced8d6c
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-02-25 14:00:51 +0100

    feat(main): implement files and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,103 @@
 // Implement the file system kernel module
 use fuser::{Request, KernelConfig};
 use std::env;
+use std::path::PathBuf;
 use std::ffi::OsStr;
 use core::ffi::c_int;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use fuser::FileType as FType;
+
+struct File<'a> {
+    name: &'a OsStr,
+    ty: fuser::FileType,
+    inode: u64,
+    //content: Vec<UserProcess>, ou Temperature ou etc.
+    // Design ideas: one File struct and one Dir struct which both implement a func to compute
+    // its size. And also a field with its content, which is a Vec of its representing struct?
+}
+
+impl<'a> File<'a> {
+    fn new(name: &'a OsStr, ty: fuser::FileType, inode: u64) -> Self {
+        File {
+            name,
+            ty,
+            inode,
+            //content,
+        }
+    }
+
+    fn get_perms(&self) -> u16 {
+        // Get the file permissions
+
+        match self.ty {
+            fuser::FileType::Directory => 0o755,
+            fuser::FileType::RegularFile => 0o644,
+            _ => 0o000,
+        }
+    }
+}
 
 struct UserProcFS {
     // Initialize the UserProcFS
+    
+    // Mountpoint
+    mountpoint: PathBuf,
+
+    // Files
+    files: Vec<File<'static>>,
 }
 
 impl UserProcFS {
-    fn new() -> Self {
-        UserProcFS {
-            // Initialize the UserProcFS
+    fn new(mountpoint: PathBuf) -> Self {
+        // Initialize the UserProcFS
+        let mut new_userprocfs = UserProcFS {
+            mountpoint: mountpoint,
+            files: Vec::new()
+        };
+        new_userprocfs.files.push(File::new(OsStr::new("."), FType::Directory, 1));
+        new_userprocfs.files.push(File::new(OsStr::new(".."), FType::Directory, 2));
+        new_userprocfs.files.push(File::new(OsStr::new("processes"), FType::Directory, 3));
+        new_userprocfs.files.push(File::new(OsStr::new("temperatures"), FType::RegularFile, 4));
+        new_userprocfs.files.push(File::new(OsStr::new("memory"), FType::RegularFile, 5));
+        new_userprocfs.files.push(File::new(OsStr::new("network"), FType::RegularFile, 6));
+        new_userprocfs.files.push(File::new(OsStr::new("disk"), FType::RegularFile, 7));
+        new_userprocfs.files.push(File::new(OsStr::new("cpu"), FType::RegularFile, 8));
+        new_userprocfs
+    }
+
+    fn get_file_by_inode(&self, inode: u64) -> Option<&File> {
+        // Get the file by inode
+
+        match inode {
+            1 => Some(&self.files[0]),
+            2 => Some(&self.files[1]),
+            3 => Some(&self.files[2]),
+            4 => Some(&self.files[3]),
+            5 => Some(&self.files[4]),
+            6 => Some(&self.files[5]),
+            7 => Some(&self.files[6]),
+            8 => Some(&self.files[7]),
+            _ => None,
+        }
+    }
+
+    fn get_file_by_name(&self, name: &OsStr) -> Option<&File> {
+        // Get the file by name
+
+        if name == "processes" {
+            Some(&self.files[0])
+        } else if name == "temperatures" {
+            Some(&self.files[1])
+        } else if name == "memory" {
+            Some(&self.files[2])
+        } else if name == "network" {
+            Some(&self.files[3])
+        } else if name == "disk" {
+            Some(&self.files[4])
+        } else if name == "cpu" {
+            Some(&self.files[5])
+        } else {
+            None
         }
     }
 
@@ -30,191 +115,82 @@ impl fuser::Filesystem for UserProcFS {
     fn getattr(&mut self, _req: &Request, ino: u64, fh: Option<u64>, reply: fuser::ReplyAttr) {
         // Get the file attributes
         
-        let now = SystemTime::now();
+        let file = UserProcFS::get_file_by_inode(&self, ino);
 
-        // Filesystem mount root directory attributes
-        if ino == 1 {
-            reply.attr(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 1,
-                size: 0,
-                blocks: 0,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::Directory,
-                perm: 0o755,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            });
-        } else if 1 <= ino && ino <= 5  {
-            reply.attr(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 2,
-                size: 99999999 as u64,
-                blocks: 0,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 1,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            });
-        } else {
-            reply.error(libc::ENOENT);
+        match file {
+            Some(file) => {
+                let now = SystemTime::now();
+                reply.attr(&Duration::from_secs(1), &fuser::FileAttr {
+                    ino: file.inode,
+                    size: 0,
+                    blocks: 0,
+                    atime: now,
+                    mtime: now,
+                    ctime: now,
+                    crtime: now,
+                    kind: file.ty,
+                    perm: file.get_perms(),
+                    nlink: 2,
+                    uid: 1000,
+                    gid: 1000,
+                    rdev: 0,
+                    blksize: 512,
+                    flags: 0,
+                });
+            },
+            None => {
+                reply.error(libc::ENOENT);
+            }
         }
     }
 
     fn lookup(&mut self, _req: &Request<'_>, parent: u64, name: &OsStr, reply: fuser::ReplyEntry) {
         // Look up a directory entry
       
-        if name == "processes" {
-            let now = SystemTime::now();
+        let file = UserProcFS::get_file_by_name(&self, name);
 
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 2,
-                size: 88888888 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::Directory,
-                perm: 0o755,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else if name == "temperatures" {
-            let now = SystemTime::now();
-
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 3,
-                size: 99999999 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else if name == "memory" {
-            let now = SystemTime::now();
-
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 4,
-                size: 99999999 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else if name == "network" {
-            let now = SystemTime::now();
-
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 5,
-                size: 99999999 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else if name == "disk" {
-            let now = SystemTime::now();
-
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 6,
-                size: 99999999 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else if name == "cpu" {
-            let now = SystemTime::now();
-
-            reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
-                ino: 7,
-                size: 99999999 as u64,
-                blocks: 1,
-                atime: now,
-                mtime: now,
-                ctime: now,
-                crtime: now,
-                kind: fuser::FileType::RegularFile,
-                perm: 0o644,
-                nlink: 2,
-                uid: 1000,
-                gid: 1000,
-                rdev: 0,
-                blksize: 512,
-                flags: 0,
-            }, 0);
-        } else {
-            reply.error(libc::ENOENT);
+        match file {
+            Some(file) => {
+                let now = SystemTime::now();
+                reply.entry(&Duration::from_secs(1), &fuser::FileAttr {
+                    ino: file.inode,
+                    size: 0,
+                    blocks: 0,
+                    atime: now,
+                    mtime: now,
+                    ctime: now,
+                    crtime: now,
+                    kind: file.ty,
+                    perm: file.get_perms(),
+                    nlink: 2,
+                    uid: 1000,
+                    gid: 1000,
+                    rdev: 0,
+                    blksize: 512,
+                    flags: 0,
+                }, 0);
+            },
+            None => {
+                reply.error(libc::ENOENT);
+            }
         }
     }
 
     fn readdir(&mut self, _req: &Request<'_>, ino: u64, fh: u64, offset: i64, mut reply: fuser::ReplyDirectory) {
         // Read the directory contents
 
-        if offset == 0 {
-            reply.add(1, 1, fuser::FileType::Directory, ".");
-            reply.add(1, 2, fuser::FileType::Directory, "..");
-            reply.add(2, 3, fuser::FileType::Directory, "processes");
-            reply.add(3, 4, fuser::FileType::RegularFile, "temperatures");
-            reply.add(4, 5, fuser::FileType::RegularFile, "memory");
-            reply.add(5, 6, fuser::FileType::RegularFile, "network");
-            reply.add(6, 7, fuser::FileType::RegularFile, "disk");
-            reply.add(7, 8, fuser::FileType::RegularFile, "cpu");
+        if ino != 1 && ino != 2 {
+            reply.error(libc::ENOENT);
+            return;
         }
+
+        for file in &self.files {
+            if reply.add(file.inode, offset + 1, file.ty, file.name) {
+                println!("Failed to add entry to directory, buffer is full.");
+                break;
+            }
+        }
+
         reply.ok();
     }
 
@@ -271,9 +247,6 @@ fn main() {
     // Threads handle
     let mut handles = Vec::new();
 
-    // Create a new instance of the file system
-    let fs = UserProcFS::new();
-
     let mountpoint = match env::args().nth(1) {
         Some(path) => path,
         None => {
@@ -281,6 +254,9 @@ fn main() {
             return;
         }
     };
+    // Create a new instance of the file system
+    let fs = UserProcFS::new(mountpoint.clone().into());
+
 
     println!("Attempting to mount the UserProcFS file system at: {:?}", mountpoint);
     let mounted = fuser::spawn_mount2(fs, mountpoint, &[fuser::MountOption::RW]);
@@ -288,6 +264,7 @@ fn main() {
     
     println!("Sleeping 8 seconds before umounting.");
     std::thread::sleep(std::time::Duration::from_secs(8));
+    // TODO : handle signals to unmount the file system.
     
     println!("Attempting to unmount the UserProcFS file system");
     UserProcFS::unmount();
